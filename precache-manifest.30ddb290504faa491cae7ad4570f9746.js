d256cd278800d28de7e8d8e66c7928d2e4831ad6
unknown <494136001@qq.com>
2018-12-18 16:29:55 +0800

    vue-douban

---
+++ precache-manifest.30ddb290504faa491cae7ad4570f9746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2894918c2d3fad3f8337",
+    "url": "/js/chunk-vendors.8e475858.js"
+  },
+  {
+    "revision": "5567962742214208e153",
+    "url": "/js/chunk-4fa1eb0e.1b5dfcf8.js"
+  },
+  {
+    "revision": "6f4b9c57dc2136af9d17",
+    "url": "/js/app.589ba9db.js"
+  },
+  {
+    "revision": "8c4a7080b91c386f9109eef612cf23aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5567962742214208e153",
+    "url": "/css/chunk-4fa1eb0e.0b7308c9.css"
+  },
+  {
+    "revision": "6f4b9c57dc2136af9d17",
+    "url": "/css/app.6559ff3a.css"
+  }
+];
